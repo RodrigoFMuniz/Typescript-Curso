e6635d0393c17b002e6ce3b4b87c985e095d8d2a
Rodrigo Fernandes Muniz <rodrigo.muniz.dev@gmail.com>
2020-10-24 12:28:46 -0300

    Aula 1 - type annotation

--- src/Aula1/type-annotation.ts
+++ src/Aula1/type-annotation.ts
@@ -1,5 +1,25 @@
 /*eslint-disable*/
+//Nunca inferir tipos antes. typeof(var) === any ? inferir tipo : não inferir tipo;
+
 let nome: string = 'Rodrigo';
 let idade: number = 30;
 let adulto: boolean = true;
-let simbolo: symbol = Symbol('qualquer-symbol')
+let simbolo: symbol = Symbol('qualquer-symbol');
+
+//Declarações de arrays
+//const arrayOfNumbers: number[] = [1,2,3];
+const arrayOfNumbers: Array<number>  = [1,2,3];
+const arrayOfStrings: Array<string>  = ['1','2','3'];
+
+//objects
+const pessoa:{nome: string, idade: number, adulto?: boolean} = {
+  nome: 'Rodrigo',
+  idade: 30,
+  adulto: true,
+};
+
+//functions
+function soma(x:number, y: number): number{
+  return x+y;
+}
+const soma2: (x: number, y:number) => number = (x, y) => x + y;
